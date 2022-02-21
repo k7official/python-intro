ac458168f873372f06166e3ddb72e7f5fa6389c3
k7official <78650685+k7official@users.noreply.github.com>
2022-02-21 12:52:55 +0800

    changed classes.py
    Added a new class to classes.py

--- classes.py
+++ classes.py
@@ -25,3 +25,26 @@ print(new_person.l_name)
 new_person.new()
 new_player = Player("Leo", "Messi", 2000)
 print(new_player.get_details())
+
+
+class Enemy:
+    life = 3
+
+    def attack(self):
+        print("ouch")
+        self.life -= 1
+
+    def checkLife(self):
+        if self.life <= 0:
+            print("I am dead")
+        else:
+            print(str(self.life) + " life left")
+
+
+enemy1 = Enemy()
+enemy2 = Enemy()
+
+enemy1.attack()
+enemy1.attack()
+enemy1.checkLife()
+enemy2.checkLife()
