928554c6463d7a3ad299e3d8309ab1f77bb8eff4
k7official <78650685+k7official@users.noreply.github.com>
2022-02-23 12:20:08 +0800

    added word_count.py
    Trying to get all words from a webpage

---
+++ word_count.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import operator
+
+
+def start(url):
+    word_list = []
+    source_code = requests.get(url).text
+    soup = BeautifulSoup(source_code, features="html.parser")
+    for post_text in soup.findAll('a', {'class': 'index_singleListingTitles'}):
+        content = post_text.string
+        words = content.lower().split()
+        for each_word in words:
+            print(each_word)
+            word_list.append(each_word)
+
+
+start('https://en.wikipedia.org/wiki/Web_scrapingr')
