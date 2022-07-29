a821be53cf67ba76289c51f71d3261134e8fcc87
k7official <78650685+k7official@users.noreply.github.com>
2022-07-29 15:44:14 +0800

    added number guessing game

---
+++ 100days/Number Guessing Game/main.py
@@ -0,0 +1,55 @@
+import random
+
+
+easy_level = 10
+hard_level = 5
+
+
+# Function to check user's guess against actual answer.
+def check_answer(guess, answer, turns):
+    """checks answer against guess. Returns the number of turns remaining."""
+    if guess > answer:
+        print("Too high.")
+        return turns - 1
+    elif guess < answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {answer}.")
+
+
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == "easy":
+        return easy_level
+    else:
+        return hard_level
+
+
+def play_game():
+    # Choosing a random number between 1 and 100.
+    print("Welcome to the Number Guessing Game")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = random.randint(1, 100)
+    print(f"The correct answer is {answer}")
+
+    turns = set_difficulty()
+    # Repeat the guessing functionality if they get it wrong.
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        # Track the number of turns and reduce by 1 if they get it wrong.
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of guesses, you lose.")
+            return
+        elif guess != answer:
+            print("Guess again.")
+
+
+play_game()
+
+
+
+
